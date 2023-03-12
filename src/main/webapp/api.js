743f28f5930b6f63f6f097618b94297ef93d3860
vixxjy <ajorvictor48@gmail.com>
2023-03-12 10:39:25 +0100

    [quick sync] updates made for biometrics

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
- export const  token = (new URLSearchParams(window.location.search)).get("jwt")
- export const url = '/api/v1/'
-//export const url =  'http://localhost:8282/api/v1/';
-//export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc3NzgyNzk0fQ.C0XVcLVv-M9piGosaC6DQpkJwqdbX7EbQBADIot3EYFTFGKo2fWL1d5CzPo1Tj5bKrHkYPWjTu5w_8-yOlSGtQ'
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
+ //export const url =  'http://localhost:8283/api/v1/';
+ //export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc4MzE4MDI1fQ.vZZH1uUER5KWakaVWwli-l0sOftExP6aasHjh5QB5vfTKg4VIrhrqxlCvAC02fxxFDGBh4AqIG_Dwj6gRAuRaQ'
